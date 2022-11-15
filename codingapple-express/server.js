8c65099cf4710ac04cba297275dfc192603ed8ab
jm4293 <dlwoals4293@gmail.com>
2022-11-16 01:30:38 +0900

    mongodb connect

--- codingapple-express/server.js
+++ codingapple-express/server.js
@@ -3,11 +3,23 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extends: true }));
 
-const MongoClient = require('mongodb').
 
-app.listen(8080, function () {
-    console.log('8080 on');
-});
+const MongoClient = require('mongodb').MongoClient
+let db;
+MongoClient.connect('mongodb+srv://admin:1q2w3e@atlascluster.yw4gtbz.mongodb.net/todoapp?retryWrites=true&w=majority', function (error, client) {
+    if (error) return console.log(error)
+
+    db = client.db('todoapp');
+
+    db.collection('post').insertOne({ 이름: 'John', _id: 100 }, function (error, result) {
+        console.log('저장완료');
+    });
+
+    app.listen(8080, function () {
+        console.log('listening on 8080')
+    })
+})
+
 
 app.get('/beauty', function (req, res) {
     res.send('beauty page');
