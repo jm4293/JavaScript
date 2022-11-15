ca1a0a670c796572e1e36520658c1284b7e15bdf
jm4293 <dlwoals4293@gmail.com>
2022-11-15 23:10:03 +0900

    MongoDB

--- codingapple-express/server.js
+++ codingapple-express/server.js
@@ -1,16 +1,29 @@
-const express = require('exress');
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extends: true }));
 
+const MongoClient = require('mongodb').
 
+app.listen(8080, function () {
+    console.log('8080 on');
+});
 
-app.get('/beauty', function(res, req){
+app.get('/beauty', function (req, res) {
     res.send('beauty page');
 })
 
-app.get('/', function(res, req){
+app.get('/write', function (req, res) {
+    res.sendFile(__dirname + '/write.html');
+})
+
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.listen(8080, function(){
-    console.log('8080 on');
-});
+app.post('/add', function (req, res) {
+    res.send('전송완료')
+    console.log(req.body.title);
+    console.log(req.body.date);
+})
+
